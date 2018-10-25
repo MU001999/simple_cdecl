170f7726cd9e7beaad6517b2548d470a1ae856ae
mu001999 <mu001999@outlook.com>
2018-10-25 09:59:07 +0800

    add SCA.h

---
+++ include/SCA.h
@@ -0,0 +1,17 @@
+#ifndef SCA_H
+#define SCA_H
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <cctype>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+
+#include "tokenizer.h"
+#include "parser.h"
+
+#endif // SCA_H
